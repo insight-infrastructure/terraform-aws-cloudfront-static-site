bbd953f0c7ca152ede9edb2be00c97af534a9cef
rob <robcannonxyz@gmail.com>
2020-09-17 13:09:26 -0700

    adding www record instead of www bucket

--- main.tf
+++ main.tf
@@ -43,24 +43,15 @@ resource "aws_route53_record" "root" {
   }
 }
 
-// TODO: RM?
-//resource "aws_route53_zone" "subdomain" {
-//  count = var.subdomain == "" ? 0 : 1
-//  name = "${var.subdomain}.${var.root_domain_name}."
-//}
-//
-//resource "aws_route53_record" "subdomain_root_records" {
-//  count = var.subdomain == "" ? 0 : 1
-//
-//  zone_id = data.aws_route53_zone.root.zone_id
-//  name = "${var.subdomain}.${var.root_domain_name}"
-//  type = "NS"
-//  ttl = "30"
-//
-//  records = [
-//    aws_route53_zone.subdomain.name_servers[0],
-//    aws_route53_zone.subdomain.name_servers[1],
-//    aws_route53_zone.subdomain.name_servers[2],
-//    aws_route53_zone.subdomain.name_servers[3],
-//  ]
-//}
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.root.zone_id
+
+  name = "www.${var.subdomain}"
+  type = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.root_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.root_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
--- s3.tf
+++ s3.tf
@@ -1,30 +1,3 @@
-resource "aws_s3_bucket" "www" {
-  bucket = "www.${local.s3_bucket_name}"
-
-  acl           = "public-read"
-  force_destroy = true
-  policy        = <<POLICY
-{
-  "Version":"2012-10-17",
-  "Statement":[
-    {
-      "Sid":"AddPerm",
-      "Effect":"Allow",
-      "Principal": "*",
-      "Action":["s3:GetObject"],
-      "Resource":["arn:aws:s3:::www.${local.s3_bucket_name}/*"]
-    }
-  ]
-}
-POLICY
-
-  website {
-    redirect_all_requests_to = "https://${local.fqdn}"
-  }
-
-  tags = var.tags
-}
-
 resource "aws_s3_bucket" "root" {
   bucket = local.s3_bucket_name
   acl    = "public-read"
